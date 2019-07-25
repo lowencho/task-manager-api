9ee3599181a6ae8582c3c2b3f3dcae55bf0540d8
lowencho <renzo.leynes22@gmail.com>
2019-07-25 22:01:09 +0800

    Adding task to the DOM test

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,22 +1,8 @@
 const createForm = document.querySelector("form");
 const inputDesc = document.querySelector("#desc-input");
 const inputComp = document.querySelector("#comp-input");
+const tasks = document.querySelector("#tasks");
 
-// const readTask = () => {
-//   axios.get("http://localhost:3000/tasks");
-// };
-
-// const headers = {
-//   headers: { Authorization: `Bearer ${localStorage.getItem("token")}` }
-// };
-// var headers = {
-//   Authorization: "Bearer " + localStorage.getItem("token"),
-//   Accept: "application/json, text/plain, */*",
-//   "Content-Type": "application/x-www-form-urlencoded"
-// };
-// const token = JSON.parse(localStorage.getItem("token"));
-// const objHeader = { Authorization: "Bearer " };
-// const authentication = JSON.stringify(objHeader);
 const token = localStorage.getItem("token");
 
 createForm.addEventListener("submit", e => {
@@ -37,6 +23,25 @@ createForm.addEventListener("submit", e => {
     )
     .then(response => {
       console.log(response);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+window.addEventListener("load", e => {
+  axios
+    .get("http://localhost:3000/tasks", {
+      headers: {
+        Authorization: "Bearer " + token
+      }
+    })
+    .then(response => {
+      console.log(response.data);
+      const allTask = response.data;
+      allTask.forEach(task => {
+        console.log(task.description);
+      });
     })
     .catch(error => {
       console.log(error);
