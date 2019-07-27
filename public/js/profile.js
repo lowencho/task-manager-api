531edffaedba009666ab135349ff8370b1f3d711
lowencho <renzo.leynes22@gmail.com>
2019-07-28 00:34:32 +0800

    Upload photo and Log out button update

--- public/js/profile.js
+++ public/js/profile.js
@@ -5,12 +5,13 @@ const tasks = document.querySelector("#tasks");
 const deleteTask = document.querySelector("#delete");
 const updateTask = document.querySelector("#update");
 const inputID = document.querySelector("#id");
+const choosePhoto = document.querySelector("#choose-image-upload");
+const uploadPhoto = document.querySelector("#image-upload");
 
 const token = localStorage.getItem("token");
 
 //View
 const taskUl = document.querySelector("#ul");
-const taskComp = document.querySelector("span");
 
 //Logout
 const logoutButton = document.querySelector("#logout");
@@ -93,9 +94,10 @@ window.addEventListener("load", e => {
 
 //Logout User
 logoutButton.addEventListener("click", e => {
+  const dataBody = {};
   console.log("Clicked");
   axios
-    .post("http://localhost:3000/users/logout", {
+    .post("http://localhost:3000/users/logout", dataBody, {
       headers: {
         Authorization: "Bearer " + token
       }
@@ -107,4 +109,16 @@ logoutButton.addEventListener("click", e => {
     .catch(error => {
       console.log(error);
     });
+});
+
+//Upload image
+uploadPhoto.addEventListener("click", e => {
+  const formData = new FormData();
+  formData.append("avatar", choosePhoto.files[0]);
+  axios.post("http://localhost:3000/users/me/avatar", formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+      Authorization: "Bearer " + token
+    }
+  });
 });
